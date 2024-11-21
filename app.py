6b9393c0a18aed25282de3c4aea4245a3ffe00cd
soikat <soikatpalcs@gmail.com>
2024-11-21 20:44:14 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        # Handle form submission
+        return "Message sent!"
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
